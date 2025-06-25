1e7d73e769e5da3b65337c4d94f45d96792454ad
pascal <pascal.seitz@gmail.com>
2025-06-25 21:12:45 +0200

    use stacker

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,25 +1,31 @@
 use fnv::FnvHashMap;
+use stacker::ArenaHashMap;
 
 use crate::{fingerprint::fingerprint, tokenizer::tokenize};
 
 pub struct PreliminaryIndex {
-    pub term_hash_map: FnvHashMap<String, u32>,
+    pub term_hash_map: ArenaHashMap,
     pub preliminary_docs: Vec<PreliminaryDoc>,
 }
 
 pub fn preliminary_index(lines: impl Iterator<Item = String>) -> PreliminaryIndex {
-    let mut term_hash_map: FnvHashMap<String, u32> = FnvHashMap::default();
+    let mut term_hash_map = ArenaHashMap::with_capacity(4);
     let mut preliminary_docs: Vec<PreliminaryDoc> = Vec::new();
     for line in lines {
         let mut token_type_with_term_ids: Vec<(u8, u32)> = Vec::new();
         let tokens = tokenize(&line);
         let fingerprint = fingerprint(&tokens);
         for token in tokens {
-            let next_id = term_hash_map.len() as u32;
-            let term_id = term_hash_map
-                .entry(token.as_str().to_string())
-                .or_insert(next_id);
-            token_type_with_term_ids.push((token.type_id(), *term_id));
+            let next_id: u32 = term_hash_map.len() as u32;
+            let mut term_id = 0;
+            term_hash_map.mutate_or_create(token.as_str().as_bytes(), |value: Option<u32>| {
+                term_id = if let Some(id) = value { id } else { next_id };
+                term_id
+            });
+            //let term_id = term_hash_map
+            //.entry(token.as_str().to_string())
+            //.or_insert(next_id);
+            token_type_with_term_ids.push((token.type_id(), term_id));
         }
         preliminary_docs.push(PreliminaryDoc::new(token_type_with_term_ids, fingerprint));
     }
