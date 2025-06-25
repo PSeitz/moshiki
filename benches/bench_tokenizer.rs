f7788501e8f45b7842bfc2cfd0b3a1b4b8b2274a
pascal <pascal.seitz@gmail.com>
2025-06-25 20:59:49 +0200

    add bench

--- benches/bench_tokenizer.rs
+++ benches/bench_tokenizer.rs
@@ -1,5 +1,5 @@
 use binggan::{BenchRunner, INSTRUMENTED_SYSTEM, PeakMemAlloc, black_box, plugins::*};
-use moshiki::tokenizer::Tokenizer;
+use moshiki::{patterns::preliminary_index, tokenizer::Tokenizer};
 
 #[global_allocator]
 pub static GLOBAL: &PeakMemAlloc<std::alloc::System> = &INSTRUMENTED_SYSTEM;
@@ -10,10 +10,10 @@ fn test_tokenizer(lines: &Vec<&str>) -> u32 {
         .map(|line| Tokenizer::new(line).count() as u32)
         .sum()
 }
+const LOG_LINES: &str = include_str!("../hdfs-logs");
 
-fn run_bench() {
-    let log_lines = include_str!("../hdfs-logs");
-    let inputs: Vec<(&str, Vec<&str>)> = vec![("hdfs-logs", log_lines.lines().collect())];
+fn bench_tokenizer() {
+    let inputs: Vec<(&str, Vec<&str>)> = vec![("hdfs-logs", LOG_LINES.lines().collect())];
     let mut runner: BenchRunner = BenchRunner::new();
 
     runner
@@ -25,7 +25,7 @@ fn run_bench() {
         group.set_name(input_name);
         let input_size = data.iter().map(|line| line.len()).sum::<usize>();
         group.set_input_size(input_size);
-        group.register_with_input("vec", data, move |data| {
+        group.register_with_input("tokenizer", data, move |data| {
             let num_tokens = black_box(test_tokenizer(data));
             num_tokens as u64
         });
@@ -33,6 +33,28 @@ fn run_bench() {
     }
 }
 
+fn bench_mini_index() {
+    let inputs: Vec<(&str, &str)> = vec![("hdfs-logs", LOG_LINES)];
+    let mut runner: BenchRunner = BenchRunner::new();
+
+    runner
+        .add_plugin(CacheTrasher::default())
+        .add_plugin(PeakMemAllocPlugin::new(GLOBAL));
+
+    for (input_name, data) in inputs.iter() {
+        let mut group = runner.new_group();
+        group.set_name(input_name);
+        let input_size = data.len();
+        group.set_input_size(input_size);
+        group.register_with_input("mini index", data, move |data| {
+            let mini_index = black_box(preliminary_index(data.lines().map(|s| s.to_string())));
+            mini_index.preliminary_docs.len() as u64
+        });
+        group.run();
+    }
+}
+
 fn main() {
-    run_bench();
+    bench_tokenizer();
+    bench_mini_index();
 }
